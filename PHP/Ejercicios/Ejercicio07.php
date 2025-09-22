36313d8c7492ea54971e44837dd7f88edb2b567c
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2025-09-22 17:48:02 +0200

    Ejercicios PHP

--- PHP/Ejercicios/Ejercicio07.php
+++ PHP/Ejercicios/Ejercicio07.php
@@ -2,14 +2,29 @@
 <html lang="es">
 <?php
 
-$month = 8;
-$year = 2025;
+// Crear un array vacío para los errores
+$errors = [];
 
-$month = $_GET ?? '';
+$month = $_GET['m'] ?? '';
 $month = filter_var($month, FILTER_VALIDATE_INT, ["options" => ["min_range" => 1, "max_range" => 12]]);
-$year = $_GET ?? '';
-$year = filter_var($year, FILTER_VALIDATE_INT, ["options" => ["min_range" => 2020, "max_range" => 2030]]);
+if ($month === false) {
+    array_push($errors, "El mes tiene que ser un número entre 1 y 12 ambos incluidos.");
+}
 
+$year = $_GET['y'] ?? '';
+$year = filter_var($year, FILTER_VALIDATE_INT, ["options" => ["min_range" => 2020, "max_range" => 2030]]);
+if ($year === false) {
+    array_push($errors, "El año tiene que estar entre el 2020 y el 2030 ambos incluidos");
+}
+
+
+
+$pageTitle = '';
+if (empty($errors)) {
+    $pageTitle = "Calendario de $month/$year";
+} else {
+    $pageTitle = "Calendario - Se han producido errores";
+}
 
 
 ?>
@@ -17,7 +32,7 @@ $year = filter_var($year, FILTER_VALIDATE_INT, ["options" => ["min_range" => 202
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calendario de <?= "$month/$year" ?></title>
+    <title><?= $pageTitle ?></title>
     <style>
         table {
             border-collapse: collapse;
@@ -30,52 +45,59 @@ $year = filter_var($year, FILTER_VALIDATE_INT, ["options" => ["min_range" => 202
 </head>
 
 <body>
-    <h1> Calendario de <?= "$month/$year" ?></h1>
-    <?php
-    $firstDayOfMonth = DateTime::createFromFormat("Y-m-d", "$year-$month-1");
-    $numberOfDaysInMonth = $firstDayOfMonth->format("t");
-    $startWeekDay = $firstDayOfMonth->format("w");
-    ?>
-    <p>
-        <?php var_dump($firstDayOfMonth) ?>
-    </p>
-    <p>
-        <?php var_dump($numberOfDaysInMonth) ?>
-    </p>
-    <p>
-        <?php var_dump($startWeekDay) ?>
-    </p>
+    <?php if (empty($errors)) { ?>
+        <h1> Calendario de <?= "$month/$year" ?></h1>
+        <?php
+        $firstDayOfMonth = DateTime::createFromFormat("Y-m-d", "$year-$month-1");
+        $numberOfDaysInMonth = $firstDayOfMonth->format("t");
+        $startWeekDay = $firstDayOfMonth->format("w");
+        ?>
 
-    <table>
-        <tr>
-            <?php
-            $currentDay = 1;
-            $daysOfCurrentWeek = 0;
 
-            while ($daysOfCurrentWeek <  $startWeekDay - 1) {
-                echo "<td></td>";
-                $daysOfCurrentWeek++;
-            }
+        <table>
+            <tr>
+                <?php
+                $currentDay = 1;
+                $daysOfCurrentWeek = 0;
 
-            while ($currentDay <= $numberOfDaysInMonth) {
-                echo "<td>$currentDay</td>";
-                $currentDay++;
-                $daysOfCurrentWeek++;
-                if ($daysOfCurrentWeek >= 7) {
-                    echo "</tr><tr>";
-                    $daysOfCurrentWeek = 0;
-                }
-            }
-            if ($daysOfCurrentWeek > 0)
-                while ($daysOfCurrentWeek++ < 7) {
+                while ($daysOfCurrentWeek <  $startWeekDay - 1) {
                     echo "<td></td>";
+                    $daysOfCurrentWeek++;
                 }
 
+                while ($currentDay <= $numberOfDaysInMonth) {
+                    echo "<td>$currentDay</td>";
+                    $currentDay++;
+                    $daysOfCurrentWeek++;
+                    if ($daysOfCurrentWeek >= 7) {
+                        echo "</tr><tr>";
+                        $daysOfCurrentWeek = 0;
+                    }
+                }
+                if ($daysOfCurrentWeek > 0)
+                    while ($daysOfCurrentWeek++ < 7) {
+                        echo "<td></td>";
+                    }
 
-            ?>
-        </tr>
 
-    </table>
+                ?>
+            </tr>
+
+        </table>
+    <?php
+    } else {
+    ?>
+        <h1> Calendario - Con errores</h1>
+        <p>Se han producido <?= sizeof($errors) ?> errores:</p>
+        <ul>
+            <?php foreach ($errors as $error): ?>
+                <li><?= $error ?></li>
+            <?php endforeach ?>
+        </ul>
+
+    <?php
+    }
+    ?>
 
 
 
